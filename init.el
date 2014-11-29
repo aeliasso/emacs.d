a863e405d5a132acc1d9ecadb7b63e4a57a257ed
Anton Eliasson <devel@antoneliasson.se>
2014-11-29 14:24:24 +0100

    Config import from .emacs

---
+++ init.el
@@ -0,0 +1,36 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(create-lockfiles nil)
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(fill-column 80)
+ '(glyphless-char-display-control (quote ((format-control . acronym) (no-font . hex-code))))
+ '(inhibit-startup-screen t)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (3 ((shift) . 1) ((control)))))
+ '(scroll-conservatively 10)
+ '(sentence-end-double-space nil)
+ '(tab-width 4)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; User local load path
+; Config
+(add-to-list 'load-path "~/.emacs.d/config/")
+; Libraries
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+
+(load-library "emacs-cfg")
+(load-library "latex-cfg")
+(load-library "lua-cfg")
+(load-library "org-cfg")
+(load-library "scala-cfg")
+(load-library "vc-cfg")
