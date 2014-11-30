085c77f7efa2c5415cde9334c82a7e9cb2d35127
Anton Eliasson <devel@antoneliasson.se>
2014-11-30 10:30:19 +0100

    C-x C-t to transpose paragraphs in LaTeX-mode

--- config/latex-cfg.el
+++ config/latex-cfg.el
@@ -4,3 +4,9 @@
 ; Generera PDF istället för DVI. latex kommer fortfarande användas istället för
 ; pdflatex, tror jag, men det verkar inte spela någon roll.
 (setq TeX-PDF-mode t)
+
+; Lines aren't really a concept in LaTeX, so it usually makes no sense to
+; transpose them. Paragraphs on the other hand:
+(add-hook 'latex-mode-hook
+          (lambda ()
+            (define-key latex-mode-map (kbd "C-x C-t") 'transpose-paragraphs)))
