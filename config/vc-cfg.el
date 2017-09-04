924bb93f05f2a899b518a1d66a4a271206543822
Anton Eliasson <antone@axis.com>
2017-09-04 12:47:34 +0200

    Add magit and magit-gerrit configuration donated by olani

--- config/vc-cfg.el
+++ config/vc-cfg.el
@@ -1,4 +1,23 @@
-;(use-package magit)
+(use-package magit-gerrit
+  :ensure t
+  :defer t
+  :commands (magit-gerrit-mode magit-gerrit-popup)
+  :defines (magit-gerrit-popup-prefix magit-gerrit-ssh-creds)
+  :init
+  (setq magit-gerrit-popup-prefix "Q")
+  (setq-default magit-gerrit-ssh-creds "antone@gittools.se.axis.com")
+  )
+
+(use-package magit
+  :ensure t
+  :commands (magit-status magit-define-popup-action)
+  :bind (("<menu>" . magit-status)
+		 :map magit-status-mode-map
+		 ("M-q" . magit-gerrit-mode))
+;:config (require 'coan-magit)
+;(add-hook 'magit-status-mode-hook #'coan-add-magit-gerrit-popup-key t)
+  )
+
 
 ;; Save files under version control automatically
 ;(require 'real-auto-save)
