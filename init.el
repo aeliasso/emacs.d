3ed59940a869a24a56a254df95451859a8377e68
Anton Eliasson <antone@axis.com>
2019-10-21 11:36:05 +0200

    Add package dts-mode

--- init.el
+++ init.el
@@ -52,7 +52,7 @@
      ("onpa" . "https://olanilsson.bitbucket.io/packages/"))))
  '(package-selected-packages
    (quote
-    (meson-mode bitbake-modes ggtags magit use-package markdown-mode meson-mode)))
+    (dts-mode meson-mode bitbake-modes ggtags magit use-package markdown-mode meson-mode)))
  '(revert-without-query (quote (".*")))
  '(scroll-conservatively 10)
  '(sentence-end-double-space nil)
