8e836c51bb2c82c4df108c5c05333dc93483be8d
Anton Eliasson <devel@antoneliasson.se>
2020-03-11 21:17:52 +0100

    Add rust-mode to selected packages

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
      ("onpa" . "https://olanilsson.bitbucket.io/packages/"))))
  '(package-selected-packages
    (quote
-    (kconfig-mode dts-mode meson-mode bitbake-modes ggtags magit use-package markdown-mode meson-mode)))
+    (rust-mode kconfig-mode dts-mode meson-mode bitbake-modes ggtags magit use-package markdown-mode meson-mode)))
  '(revert-without-query (quote (".*")))
  '(scroll-conservatively 10)
  '(sentence-end-double-space nil)
