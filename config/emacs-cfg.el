0dd2306fdfde7ffd63d36832ab4746fef9c276dd
Anton Eliasson <antone@axis.com>
2020-05-08 15:19:19 +0200

    emacs-cfg: Add ediff function to copy both A and B to C

--- config/emacs-cfg.el
+++ config/emacs-cfg.el
@@ -106,3 +106,13 @@
 	    (setq indent-tabs-mode nil)
 	    (local-unset-key (kbd "M-<left>"))
 	    (local-unset-key (kbd "M-<right>"))))
+
+;; https://stackoverflow.com/questions/9656311/conflict-resolution-with-emacs-ediff-how-can-i-take-the-changes-of-both-version/29757750#29757750
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
