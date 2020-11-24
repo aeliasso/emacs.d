dca57428f71f59d75ff0c6feee645b5f89160260
Anton Eliasson <devel@antoneliasson.se>
2020-11-24 16:03:27 +0100

    Switch to the new buffer after grepping
    This includes magit-grep.

--- config/emacs-cfg.el
+++ config/emacs-cfg.el
@@ -125,3 +125,7 @@
                     (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
 (defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
 (add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
+(add-hook 'grep-mode-hook
+          '(lambda ()
+             (switch-to-buffer-other-window "*grep*")))
