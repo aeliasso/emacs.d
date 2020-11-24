7063ad33d0715822351f65a94e57de325dbac7cf
Anton Eliasson <devel@antoneliasson.se>
2020-11-24 16:04:23 +0100

    Make the links clickable in Magit Process buffers

--- config/vc-cfg.el
+++ config/vc-cfg.el
@@ -3,6 +3,9 @@
 
 (defalias 'magit-grep 'vc-git-grep)
 
+;; Make the links in Gerrit's responses clickable
+(add-hook 'magit-process-mode-hook 'goto-address-mode)
+
 ;; Save files under version control automatically
 ;(require 'real-auto-save)
 ; ... how?
