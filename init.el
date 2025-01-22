cb5a70cb9b18e664af2c541d09809078e24a2746
Anton Eliasson <anton.eliasson@intel.com>
2025-01-22 15:44:32 +0100

    Enable hl-line-mode in compilation-mode

--- init.el
+++ init.el
@@ -13,6 +13,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(compilation-mode-hook '(hl-line-mode))
  '(compilation-scroll-output 'first-error)
  '(create-lockfiles nil)
  '(cursor-type 'bar)
